0e6af6349003da8c983f80fab328069530fba6ce
German Fernandez <germanfernandez0306@gmail.com>
2022-05-27 20:33:53 -0300

    feat: add commands history

--- source/main.js
+++ source/main.js
@@ -6,6 +6,9 @@ const language = 'es';
 const $display = $('#display');
 const $input = $('#read');
 
+let commandsHistory = [];
+let currentIndex = -1;
+
 (function () {
   displayMessage($display, CONSOLE_MESSAGES.BOOT[language]);
 })();
@@ -15,7 +18,24 @@ $input.addEventListener('keyup', (event) => {
     const inputValue = $input.value;
     inputValue.trim() === '' ? null : displayInputMessage($display, inputValue);
     $input.value = '';
+    commandsHistory = [inputValue, ...commandsHistory];
+    currentIndex = -1;
     return;
+  }
+
+  if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
+    if (commandsHistory.length === 0) {
+      return;
+    }
+
+    event.key === 'ArrowUp' ? currentIndex++ : currentIndex--;
+    if (currentIndex > commandsHistory.length - 1) {
+      currentIndex = commandsHistory.length - 1;
+    } else if (currentIndex < 0) {
+      currentIndex = 0;
+    }
+
+    $input.value = commandsHistory[currentIndex];
   }
 });
 
