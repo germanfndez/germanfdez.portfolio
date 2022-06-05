960c5f18586e8a3d09ae5a07c0d933c09a26f032
German Fernandez <germanfernandez0306@gmail.com>
2022-06-05 19:26:01 -0300

    feat: add beta data

--- source/data.js
+++ source/data.js
@@ -14,11 +14,30 @@ export const CONSOLE_MESSAGES = {
 
 export const COMMANDS = {
   ['help']: (args) => {
-    return `<p class="text light-green">Available commands:</p>
+    return `<p class="text green">Available commands:</p>
       <p class="text light-green italic">help - Show available commands</p>
-      <p class="text light-green italic">clear - Clear terminal</p>
       <p class="text light-green italic">about - Show German information</p>
       <p class="text light-green italic">contact - Show contact information</p>
+      <p class="text light-green italic">projects - Show my projects</p>
+      <p class="text light-green italic">clear - Clear the console</p>
+      `;
+  },
+  ['about']: (args) => {
+    return `<p class="text light-green">I am <span class="green">German</span>, a young boy who like programming. I am really keen on <span class="green">Web Development</span>, more specifically <span class="green">Backend</span>.</p>`;
+  },
+  ['contact']: (args) => {
+    return `<p class="text light-green">Sites you can contact me:</p>
+      <a class="text light-green italic" href="https://github.com/Germancitoz" target="_blank">Github</a>
+      <a class="text light-green italic" href="mailto: germanfernandez@gmail.com" target="_blank">Mail</a>
+      <a class="text light-green italic" href="https://discord.com/users/546732670005149706" target="_blank">Discord</a>
+      `;
+  },
+  ['projects']: (args) => {
+    return `<p class="text light-green">Popular projects:</p>
+    <p class="text light-green italic"><a class="text light-green italic" href="https://github.com/Germancitoz/easylogger" target="_blank">easylogger</a>- Lightweight and configurable JavaScript logger</p>
+    <p class="text light-green italic"><a class="text light-green italic" href="https://github.com/Germancitoz/users.manager.api" target="_blank">userManagerAPI</a>- Simple API for use on Frontend, to manage users using MongoDB, NodeJS(Express), JSDoc</p>
+    <p class="text light-green italic"><a class="text light-green italic" href="https://github.com/Germancitoz/MPass" target="_blank">MPass</a>- Basic Python (Tkinter) password manager</p>
+    <p class="text light-green italic">You can see more on my <a class="text green italic" href="https://github.com/Germancitoz?tab=repositories" target="_blank">Github</a></p>
     `;
   },
   ['clear']: (args) => {
