ec3e33b7b1214698b091802bc9243ec894f9ba74
German Fernandez <germanfernandez0306@gmail.com>
2024-03-31 20:56:02 -0300

    asdasdasd

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#000000',
+        secondary: '#ededed',
+        tertiary: '#1f1f1f',
+      },
+    },
+  },
+  plugins: [],
+}
